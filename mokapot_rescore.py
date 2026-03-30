bd98d07a1fbf522ef0cfe1de7f6e9cd3e84f1085
Micha Birklbauer <19354640+michabirklbauer@users.noreply.github.com>
2026-03-30 11:42:08 +0200

    update rescoring script

--- mokapot_rescore.py
+++ mokapot_rescore.py
@@ -19,10 +19,8 @@ try:
 except ImportError:
     from typing_extensions import Literal
 
-POSSIBLE_RESCORING_FEATURES = [
+PQVALUES = [
     "PG.Cscore",
-    "EG.Cscore",
-    "FG.Charge",
     "PG.Pvalue",
     "PG.PValue (Run-Wise)",
     "PG.Qvalue",
@@ -37,6 +35,10 @@ POSSIBLE_RESCORING_FEATURES = [
     "EG.MinProfileQvalue",
     "EG.PercentileQvalue",
     "FG.Qvalue",
+]
+POSSIBLE_RESCORING_FEATURES = [
+    "EG.Cscore",
+    "FG.Charge",
     "PP.MatchedIonsA",
     "PP.TotalIonsA",
     "PP.MatchedIonsB",
@@ -97,7 +99,7 @@ POSSIBLE_RESCORING_FEATURES_BETA = [
     "PP.NormalizedCrosslinkFragmentsBeta",
 ]
 
-__version = "1.0.2"
+__version = "1.1.0"
 logger = logging.getLogger(__name__)
 
 
@@ -151,10 +153,15 @@ def __do_mokapot_columns(
     return df
 
 
-def __rescore_csms(orig_df: pd.DataFrame) -> pd.DataFrame:
+def __rescore_csms(
+    orig_df: pd.DataFrame, use_p_and_q_values: bool = False
+) -> pd.DataFrame:
     df = __do_mokapot_columns(orig_df)
+    possible_rescoring_features = POSSIBLE_RESCORING_FEATURES
+    if use_p_and_q_values:
+        possible_rescoring_features += PQVALUES
     rescoring_features = [
-        feature for feature in POSSIBLE_RESCORING_FEATURES if feature in df
+        feature for feature in possible_rescoring_features if feature in df
     ]
     psms = mokapot.dataset.LinearPsmDataset(
         psms=df,
@@ -177,7 +184,7 @@ def __rescore_csms(orig_df: pd.DataFrame) -> pd.DataFrame:
     return df
 
 
-def __resore_psms(orig_df: pd.DataFrame) -> pd.DataFrame:
+def __rescore_psms_separately(orig_df: pd.DataFrame) -> pd.DataFrame:
     df = orig_df.copy()
     df_alpha = __do_mokapot_columns(df, alpha=True)
     df_beta = __do_mokapot_columns(df, alpha=False)
@@ -219,9 +226,173 @@ def __resore_psms(orig_df: pd.DataFrame) -> pd.DataFrame:
     return df
 
 
+def __rescore_psms_merged(orig_df: pd.DataFrame) -> pd.DataFrame:
+    df = orig_df.copy()
+    df_alpha = __do_mokapot_columns(df, alpha=True)
+    df_beta = __do_mokapot_columns(df, alpha=False)
+    df_alpha["MERGE.CSMID"] = range(df_alpha.shape[0])
+    df_beta["MERGE.CSMID"] = range(df_beta.shape[0])
+    df_alpha.rename(
+        columns={
+            "PP.MatchedIonsA": "PP.MatchedIons",
+            "PP.TotalIonsA": "PP.TotalIons",
+            "PP.RelativeMatchScoreA": "PP.RelativeMatchScore",
+            "PP.PartialCscoreA": "PP.PartialCscore",
+            "PP.SequenceCoverageNTermAlpha": "PP.SequenceCoverageNTerm",
+            "PP.SequenceCoverageCTermAlpha": "PP.SequenceCoverageCTerm",
+            "PP.SequenceCoverageAlpha": "PP.SequenceCoverage",
+            "PP.UniScoreAlpha": "PP.UniScore",
+            "PP.PepLenAlpha": "PP.PepLen",
+            "PP.NumberCrosslinkFragmentsAlpha": "PP.NumberCrosslinkFragments",
+            "PP.NormalizedCrosslinkFragmentsAlpha": "PP.NormalizedCrosslinkFragments",
+        },
+        inplace=True,
+    )
+    df_beta.rename(
+        columns={
+            "PP.MatchedIonsB": "PP.MatchedIons",
+            "PP.TotalIonsB": "PP.TotalIons",
+            "PP.RelativeMatchScoreB": "PP.RelativeMatchScore",
+            "PP.PartialCscoreB": "PP.PartialCscore",
+            "PP.SequenceCoverageNTermBeta": "PP.SequenceCoverageNTerm",
+            "PP.SequenceCoverageCTermBeta": "PP.SequenceCoverageCTerm",
+            "PP.SequenceCoverageBeta": "PP.SequenceCoverage",
+            "PP.UniScoreBeta": "PP.UniScore",
+            "PP.PepLenBeta": "PP.PepLen",
+            "PP.NumberCrosslinkFragmentsBeta": "PP.NumberCrosslinkFragments",
+            "PP.NormalizedCrosslinkFragmentsBeta": "PP.NormalizedCrosslinkFragments",
+        },
+        inplace=True,
+    )
+    df_alpha.drop(
+        columns=list(
+            set(df_alpha.columns.tolist())
+            - {
+                "EG.Cscore",
+                "PP.MatchedIons",
+                "PP.TotalIons",
+                "PP.RelativeMatchScore",
+                "PP.PartialCscore",
+                "PP.SequenceCoverageNTerm",
+                "PP.SequenceCoverageCTerm",
+                "PP.SequenceCoverage",
+                "PP.UniScore",
+                "PP.PepLen",
+                "PP.NumberCrosslinkFragments",
+                "PP.NormalizedCrosslinkFragments",
+                "MP.Target",
+                "MP.Spectrum",
+                "MP.Peptide",
+                "MP.Protein",
+                "MERGE.CSMID",
+            }
+        ),
+        inplace=True,
+    )
+    df_beta.drop(
+        columns=list(
+            set(df_beta.columns.tolist())
+            - {
+                "EG.Cscore",
+                "PP.MatchedIons",
+                "PP.TotalIons",
+                "PP.RelativeMatchScore",
+                "PP.PartialCscore",
+                "PP.SequenceCoverageNTerm",
+                "PP.SequenceCoverageCTerm",
+                "PP.SequenceCoverage",
+                "PP.UniScore",
+                "PP.PepLen",
+                "PP.NumberCrosslinkFragments",
+                "PP.NormalizedCrosslinkFragments",
+                "MP.Target",
+                "MP.Spectrum",
+                "MP.Peptide",
+                "MP.Protein",
+                "MERGE.CSMID",
+            }
+        ),
+        inplace=True,
+    )
+    psms_df = pd.concat([df_alpha, df_beta], ignore_index=True)
+    psms_df["MERGE.PSMID"] = range(psms_df.shape[0])
+    psms_df["MP.Spectrum"] = psms_df.apply(
+        lambda row: f"{row['MP.Spectrum']}_{row['MERGE.PSMID']}", axis=1
+    )
+    dropped_features = [
+        c
+        for c in [
+            "EG.Cscore",
+            "PP.MatchedIons",
+            "PP.TotalIons",
+            "PP.RelativeMatchScore",
+            "PP.PartialCscore",
+            "PP.SequenceCoverageNTerm",
+            "PP.SequenceCoverageCTerm",
+            "PP.SequenceCoverage",
+            "PP.UniScore",
+            "PP.PepLen",
+            "PP.NumberCrosslinkFragments",
+            "PP.NormalizedCrosslinkFragments",
+        ]
+        if psms_df[c].isna().any()  # pyright: ignore[reportGeneralTypeIssues]
+    ]
+    if len(dropped_features) > 0:
+        psms_df.drop(columns=dropped_features, inplace=True)
+    logger.info(
+        f"Removed the following features because of missing values: {', '.join(dropped_features)}"
+    )
+    rescoring_features = [
+        feature
+        for feature in [
+            "EG.Cscore",
+            "PP.MatchedIons",
+            "PP.TotalIons",
+            "PP.RelativeMatchScore",
+            "PP.PartialCscore",
+            "PP.SequenceCoverageNTerm",
+            "PP.SequenceCoverageCTerm",
+            "PP.SequenceCoverage",
+            "PP.UniScore",
+            "PP.PepLen",
+            "PP.NumberCrosslinkFragments",
+            "PP.NormalizedCrosslinkFragments",
+        ]
+        if feature in psms_df
+    ]
+    psms = mokapot.dataset.LinearPsmDataset(
+        psms=psms_df,
+        target_column="MP.Target",
+        spectrum_columns="MP.Spectrum",  # pyright: ignore[reportArgumentType]
+        peptide_column="MP.Peptide",
+        protein_column="MP.Protein",
+        feature_columns=rescoring_features,
+        copy_data=True,
+        rng=1337,
+    )
+    logger.info(psms)
+    models, scores = mokapot.brew([psms])
+    scores_a = list()
+    scores_b = list()
+    scores_csm = list()
+    psms_df["Mokapot Score"] = scores[0]
+    for i in range(df.shape[0]):
+        alpha: pd.Series = psms_df.iloc[i]
+        beta: pd.Series = psms_df.iloc[df.shape[0] + i]
+        assert int(alpha["MERGE.CSMID"]) == int(beta["MERGE.CSMID"])
+        scores_a.append(alpha["Mokapot Score"])
+        scores_b.append(beta["Mokapot Score"])
+        scores_csm.append(min([alpha["Mokapot Score"], beta["Mokapot Score"]]))
+    df["Mokapot Score Alpha"] = scores_a
+    df["Mokapot Score Beta"] = scores_b
+    df["Mokapot Score"] = scores_csm
+    return df
+
+
 def rescore(
     data: str | pd.DataFrame,
     level: Literal["PSM", "CSM"] = "PSM",
+    use_p_and_q_values: bool = False,
     **kwargs,
 ) -> pd.DataFrame:
     r"""Rescore an annotated Spectronaut result file with Mokapot.
@@ -236,6 +407,9 @@ def rescore(
     level : "PSM", or "CSM", default = "PSM"
         Whether the whole CSMs (crosslink-spectrum-matches) should be rescored or
         the individual PSMs (peptide-spectrum-matches).
+    use_p_and_q_values : bool, default = False
+        Whether to use p- and q-values from Spectronaut for rescoring. Only applies
+        to level "CSM".
 
     Returns
     -------
@@ -250,17 +424,17 @@ def rescore(
     ...     level="PSM",
     ... )
     >>> df["Mokapot Score"]
-    0       -0.570398
-    1       -1.001960
-    2       -0.193422
-    3       -0.849390
-    4        0.201500
+    0       -0.679182
+    1       -1.037381
+    2       -0.197376
+    3       -0.796810
+    4        0.318801
                ...
-    14475   -2.197316
-    14476   -0.022281
-    14477   -0.333449
-    14478   -0.159475
-    14479   -0.494221
+    14475   -2.460086
+    14476   -0.099634
+    14477   -0.300225
+    14478   -0.293951
+    14479   -0.325833
     Name: Mokapot Score, Length: 14480, dtype: float64
     """
     if isinstance(data, str):
@@ -269,4 +443,4 @@ def rescore(
         df = data
     if level == "CSM":
         return __rescore_csms(df)
-    return __resore_psms(df)
+    return __rescore_psms_merged(df)
